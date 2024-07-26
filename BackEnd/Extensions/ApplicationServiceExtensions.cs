c4c7bc913f22afc33c678c6eae1817762efe49ec
adryano.chavier <adryano.chavier@senac.br>
2024-07-26 13:28:21 -0300

    Criado pasta de Extensions, limpando o codigo da program.cs

---
+++ BackEnd/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BackEnd.Data;
+using BackEnd.Interfaces;
+using BackEnd.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllers();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+ }
